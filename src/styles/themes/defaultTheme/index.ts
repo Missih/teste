027bd19b6e50fd3e7635d39aa11e55d093b5733c
Samira <grossisamira@hotmail.com>
2022-04-10 15:58:54 -0300

    feat: adds stylesheet to theme

---
+++ src/styles/themes/defaultTheme/index.ts
@@ -0,0 +1,19 @@
+import { border } from './border'
+import { brand } from './brand-colors'
+import { font } from './font'
+import { line } from './line-height'
+import { neutral } from './neutral-colors'
+import { opacity } from './opacity'
+import { shadow } from './shadow'
+import { spacing } from './spacing'
+
+export const defaultTheme = {
+    border,
+    brand,
+    font,
+    line,
+    neutral,
+    opacity,
+    shadow,
+    spacing,
+}
