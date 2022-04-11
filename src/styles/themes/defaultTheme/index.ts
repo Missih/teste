e208f77af78631f63f5de7b5a7cfc76b65251d17
Samira <grossisamira@hotmail.com>
2022-04-11 17:43:52 -0300

    refactor: changed color theme file

--- src/styles/themes/defaultTheme/index.ts
+++ src/styles/themes/defaultTheme/index.ts
@@ -1,18 +1,16 @@
 import { border } from './border'
-import { brand } from './brand-colors'
+import { color } from './colors'
 import { font } from './font'
 import { line } from './line-height'
-import { neutral } from './neutral-colors'
 import { opacity } from './opacity'
 import { shadow } from './shadow'
 import { spacing } from './spacing'
 
 export const defaultTheme = {
     border,
-    brand,
+    color,
     font,
     line,
-    neutral,
     opacity,
     shadow,
     spacing,
